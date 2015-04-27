5345164851170c7bfdb24d5c921d1dbe21a16ba7
Ihar Daineka <badvoodoolab@gmail.com>
2015-04-27 16:16:59 +0300

    Some refactoring

--- hw1/hw1.js
+++ hw1/hw1.js
@@ -1,51 +1,45 @@
-'use strict';
+(function() {
+	'use strict';
 
-const fs = require('fs'),
-	child_process = require('child_process');
-var fileName,
-	commandName;
+	var child_process = require('child_process'),
+		fs = require('fs'),
+		commandName,
+		fileName;
 
-
-if (process.argv[2] === undefined || process.argv[3] === undefined) {
-	throw 'Invalid number of arguments. Expect filename and method';
-}
-
-fileName = process.argv[2];
-commandName = process.argv[3];
-
-function copyFile() {
-	var now = new Date().toString(),
-		newFileName = fileName.split('.')[0] + '_' + now.split(' ').join('_') + '.' + fileName.split('.')[1];
-
-	console.log('File ' + fileName + ' will be copied to ' + newFileName);
-	return child_process.spawn('cp', [fileName, newFileName]);
-}
-
-function deleteFile() {
-	console.log('File ' + fileName + ' will be deleted');
-	return child_process.spawn('rm', [fileName]);
-}
-
-fs.watch(fileName, function(){
-	var action,
-		now;
-
-	console.log(fileName + ' changed!');
-
-	switch(commandName) {
-		case 'copy':
-			action = copyFile();
-			break;
-		case 'del':
-			action = deleteFile();
-			break;
-		default:
-			console.log(commandName + ': command not found');
+	if (process.argv[2] === undefined || process.argv[3] === undefined) {
+		throw 'Invalid number of arguments. Expect filename and method';
 	}
 
-	if (action) {
-		action.stdout.pipe(process.stdout);
-	}
-});
+	fileName = process.argv[2];
+	commandName = process.argv[3];
 
-console.log('Watching ' + fileName + ' changes');
+	fs.watch(fileName, function(){
+		console.log(fileName + ' changed!');
+
+		switch(commandName) {
+			case 'copy':
+				copyFile();
+				break;
+			case 'del':
+				deleteFile();
+				break;
+			default:
+				console.log(commandName + ': command not found');
+		}
+	});
+
+	function copyFile() {
+		var now = new Date().toString(),
+			newFileName = fileName.split('.')[0] + '_' + now.split(' ').join('_') + '.' + fileName.split('.')[1];
+
+		child_process.spawn('cp', [fileName, newFileName]);
+		console.log('File ' + fileName + ' copied to ' + newFileName);
+	}
+
+	function deleteFile() {
+		child_process.spawn('rm', [fileName]);
+		console.log('File ' + fileName + ' was deleted');
+	}
+
+	console.log('Watching ' + fileName + ' changes');
+})();
